17e439a50bc1c683eeef4d67440dc6cbb05c97cd
MaSStiK <matvey.ovchinnikov.2014@mail.ru>
2021-01-30 22:32:13 +0500

    Work adding

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
 $(document).ready(function() {
     
+
     $("#add").click(function() {
-        console.log(1);
-        $(".Modal__window").css("display", "flex");
+        $("#modal").show(); 
     });
 
-    $("#create").click(function() {
-        console.log(1);
-        $(".Modal__window").css("display", "none");
+    $("#close").click(function() {
+        $("#modal").hide(); 
     });
+
+    $("#new").click(function() {
+
+        let ad = {
+            text: $("input[name='text']").val(),
+            contactName: $("input[name='name']").val(),
+            contactPhone: $("input[name='phone']").val()
+        };
+
+        let adHTML = 
+        '<div class="Ads__box">' +
+            '<div class="Box__info">' + ad.text + '</div>' +
+            '<div class="Box__name">' + ad.contactName + '</div>' + 
+            '<div class="Box_phone">' + ad.contactPhone + '</div>' +
+        '</div>';
+
+        $("#content").append(adHTML);
+        $("#modal").hide(); 
+    });
+    
 
 });
