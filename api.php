637018fd8de36b4e742ad5f565e5256e9fc956dc
MaSStiK <matvey.ovchinnikov.2014@mail.ru>
2021-02-14 16:02:09 +0500

    API

---
+++ api.php
@@ -0,0 +1,8 @@
+<?php
+
+// phpinfo();
+echo '44848484 ';
+
+$array_ads = ['q' => '1'];
+
+print_r($array_ads);
--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+
+    let arrayAds = new Array();
     
 
     $("#add").click(function() {
@@ -16,17 +18,27 @@ $(document).ready(function() {
             contactName: $("input[name='name']").val(),
             contactPhone: $("input[name='phone']").val()
         };
-
-        let adHTML = 
-        '<div class="Ads__box">' +
-            '<div class="Box__info">' + ad.text + '</div>' +
-            '<div class="Box__name">' + ad.contactName + '</div>' + 
-            '<div class="Box_phone">' + ad.contactPhone + '</div>' +
-        '</div>';
-
-        $("#content").append(adHTML);
+        arrayAds.push(ad);
+        console.log(ad);
+        renderAds();
         $("#modal").hide(); 
     });
+
+
+    function renderAds() {
+        $("#content").html("");
+
+        arrayAds.map(ad => {
+            let adHTML = 
+            '<div class="Ads__box">' +
+                '<div class="Box__info">' + ad.text + '</div>' +
+                '<div class="Box__name">' + ad.contactName + '</div>' + 
+                '<div class="Box_phone">' + ad.contactPhone + '</div>' +
+            '</div>';
+
+            $("#content").append(adHTML);
+        });
+    };
     
 
 });
